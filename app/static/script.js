c61f15cff6f6ddeff940a1ba91f893d52a14c16a
jatin-blenheim <jatin.khatri@blenheimchalcot.com>
2024-03-13 12:45:16 +0530

    logout close on outside click and info tooltip added

--- app/static/script.js
+++ app/static/script.js
@@ -1,3 +1,4 @@
+const dropdownContent = document.getElementById("dropdownContent");
 const overlay = document.getElementById("overlay");
 const form = document.getElementById("upload-file-form");
 const scoreBtn = document.getElementById("score-btn");
@@ -5,7 +6,7 @@ const round2Wrapper = document.getElementById("round2-wrapper");
 const round2ScoreBtn = document.getElementById("check-round2-btn");
 const extractedTextContainer = document.querySelector(".extracted-text-container");
 const fileInput = document.querySelector("#file"),
-the_return = document.querySelector(".file-return");
+  the_return = document.querySelector(".file-return");
 const accLabel = document.getElementById("acc-label");
 const accIcon = document.querySelector(".acc-icon");
 const menuBtn = document.getElementById("menu-btn");
@@ -25,176 +26,204 @@ const round2FailReason = document.getElementById("round2-fail-reason");
 const round2FailReasonContent = document.getElementById("round2-reason-content");
 
 const toggleSidebar = () => {
-    if (sidebar.classList.contains("open")) {
-        sidebar.classList.remove("open");
-    } else {
-        sidebar.classList.add("open");
-    }
+  if (sidebar.classList.contains("open")) {
+    sidebar.classList.remove("open");
+  } else {
+    sidebar.classList.add("open");
+  }
 };
 
-function toggleDropdown() {
-    const dropdownContent = document.getElementById("dropdownContent");
-    dropdownContent.classList.toggle("show");
+// function toggleDropdown() {
+//   dropdownContent.classList.toggle("show");
+// }
+
+function toggleDropdown(event) {
+  console.log("efef");
+  dropdownContent.classList.toggle("show");
+  console.log(dropdownContent);
+  // event.stopPropogation();
+  if (dropdownContent.classList.contains("show")) {
+    document.addEventListener("click", closeDropdownOutside);
+  } else {
+    document.removeEventListener("click", closeDropdownOutside);
+  }
+}
+
+function closeDropdownOutside(event) {
+  console.log("enen");
+  console.log(event.target, document.querySelector("#three-dots"));
+  if (
+    !dropdownContent.contains(event.target) &&
+    event.target !== document.querySelector("#three-dots")
+  ) {
+    dropdownContent.classList.remove("show");
+    document.removeEventListener("click", closeDropdownOutside);
+  }
 }
 
 menuBtn.addEventListener("click", toggleSidebar);
 clonseSidebarBtn.addEventListener("click", toggleSidebar);
 
 accLabel.addEventListener("click", () => {
-    accIcon.classList.toggle("rotate");
+  accIcon.classList.toggle("rotate");
 });
 
 const setLoading = (loadingState) => {
-    if (loadingState) {
-        overlay.style.display = "flex";
-    } else {
-        overlay.style.display = "none";
-    }
+  if (loadingState) {
+    overlay.style.display = "flex";
+  } else {
+    overlay.style.display = "none";
+  }
 };
 
 fileInput.addEventListener("change", function (event) {
-    console.log("fefe");
-    const fileName = this.value.split("\\").pop();
-    the_return.innerHTML = fileName;
+  const fileName = this.value.split("\\").pop();
+  the_return.innerHTML = fileName;
+  extractedTextContainer.style.display = "none";
+  scoreBtn.style.display = "none";
+  round1TableContainer.style.display = "none";
+  round2TableContainer.style.display = "none";
+  round2Wrapper.style.display = "none";
+  round1FailReason.style.display = "none";
+  round2FailReason.style.display = "none";
+  round1Score.textContent = ``;
+  round1ScoreStatus.textContent = "";
+  round2Score.textContent = ``;
+  round2ScoreStatus.textContent = "";
+  round1FailReasonContent.textContent = "";
+  round2FailReasonContent.textContent = "";
 });
 
 form.addEventListener("submit", async (event) => {
-    event.preventDefault(); // Prevent default form submission
+  event.preventDefault(); // Prevent default form submission
 
-    setLoading(true);
-    round1TableContainer.style.display = "none";
-    round2TableContainer.style.display = "none";
-    round2Wrapper.style.display = "none";
-    round1FailReason.style.display = "none";
-    round2FailReason.style.display = "none";
-    round1Score.textContent = ``;
-    round1ScoreStatus.textContent = "";
-    round2Score.textContent = ``;
-    round2ScoreStatus.textContent = "";
-    round1FailReasonContent.textContent = "";
-    round2FailReasonContent.textContent = "";
+  setLoading(true);
 
-    const formData = new FormData();
-    var the_return = document.querySelector(".file-return");
-    const file = document.getElementById("file").files[0];
-    formData.append("file", file);
+  const formData = new FormData();
+  var the_return = document.querySelector(".file-return");
+  const file = document.getElementById("file").files[0];
+  formData.append("file", file);
 
-    try {
-        const res = await fetch("/extract-text", {
-            method: "POST",
-            body: formData,
-        });
+  try {
+    const res = await fetch("/extract-text", {
+      method: "POST",
+      body: formData,
+    });
 
-        const data = await res.json();
+    const data = await res.json();
 
-        if (res.ok) {
-            extractedTextContainer.style.display = "block";
-            const fileContent = document.getElementById("file-content");
-            fileContent.innerHTML = data.extracted_text;
-            scoreBtn.style.display = "block";
-        } else {
-            console.log(data);
-        }
-    } catch (error) {
-        console.log(error);
-    } finally {
-        setLoading(false);
+    if (res.ok) {
+      extractedTextContainer.style.display = "block";
+      const fileContent = document.getElementById("file-content");
+      fileContent.innerHTML = data.extracted_text;
+      scoreBtn.style.display = "block";
+    } else {
+      console.log(data);
     }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    setLoading(false);
+  }
 });
 
 scoreBtn.addEventListener("click", async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    setLoading(true);
+  setLoading(true);
 
-    try {
-        const res = await fetch("/analyze-pdf", {
-            method: "GET",
-        });
-        const data = await res.json();
-        console.log(data);
-        const parsedData = JSON.parse(data.data);
-        let totalScore = data.score;
+  try {
+    const res = await fetch("/analyze-pdf", {
+      method: "GET",
+    });
+    const data = await res.json();
+    console.log(data);
+    const parsedData = JSON.parse(data.data);
+    let totalScore = data.score;
 
-        while (round1tbody.firstChild) {
-            round1tbody.removeChild(round1tbody.firstChild);
-        }
-
-        parsedData.forEach(function (rowData, index) {
-            const row = document.createElement("tr");
-            const cell = document.createElement("td");
-            cell.textContent = index + 1;
-            row.appendChild(cell);
-            for (const key in rowData) {
-                const cell = document.createElement("td");
-                cell.textContent = rowData[key];
-                row.appendChild(cell);
-            }
-
-            round1tbody.appendChild(row);
-        });
-
-        round1Score.textContent = `Round 1 Score: ${totalScore}`;
-        round1TableContainer.style.display = "block";
-        if (totalScore >= 40) {
-            round1ScoreStatus.textContent = "Round 1 Cleared";
-            round1ScoreStatus.style.color = "green";
-            round2Wrapper.style.display = "block";
-        } else {
-            round1ScoreStatus.textContent = "Round 1 Failed";
-            round1ScoreStatus.style.color = "red";
-            round1FailReason.style.display = "block";
-            round1FailReasonContent.textContent = data.reason;
-        }
-    } catch (error) {
-        console.log(error);
-    } finally {
-        setLoading(false);
+    while (round1tbody.firstChild) {
+      round1tbody.removeChild(round1tbody.firstChild);
     }
+
+    parsedData.forEach(function (rowData, index) {
+      const row = document.createElement("tr");
+      const cell = document.createElement("td");
+      cell.textContent = index + 1;
+      row.appendChild(cell);
+      for (const key in rowData) {
+        const cell = document.createElement("td");
+        cell.textContent = rowData[key];
+        row.appendChild(cell);
+      }
+
+      round1tbody.appendChild(row);
+    });
+
+    round1Score.textContent = `Round 1 Score: ${totalScore}`;
+    round1TableContainer.style.display = "block";
+    if (totalScore >= 40) {
+      round1ScoreStatus.textContent = "Round 1 Cleared";
+      round1ScoreStatus.style.color = "green";
+      round2Wrapper.style.display = "block";
+    } else {
+      round1ScoreStatus.textContent = "Round 1 Failed";
+      round1ScoreStatus.style.color = "red";
+      round1FailReason.style.display = "block";
+      round1FailReasonContent.textContent = data.reason;
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    setLoading(false);
+  }
 });
 
 round2ScoreBtn.addEventListener("click", async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    setLoading(true);
+  setLoading(true);
 
-    try {
-        const res = await fetch("/check-round-two", {
-            method: "GET",
-        });
-        const data = await res.json();
-        const totalScore = data.total_score;
-        const parsedData = JSON.parse(data.data);
+  try {
+    const res = await fetch("/check-round-two", {
+      method: "GET",
+    });
+    const data = await res.json();
+    const totalScore = data.total_score;
+    const parsedData = JSON.parse(data.data);
 
-        parsedData.forEach(function (rowData, index) {
-            const row = document.createElement("tr");
-            const cell = document.createElement("td");
-            cell.textContent = index + 1;
-            row.appendChild(cell);
-            for (const key in rowData) {
-                const cell = document.createElement("td");
-                cell.textContent = rowData[key];
-                row.appendChild(cell);
-            }
-            round2tbody.appendChild(row);
-        });
-
-        round2Score.textContent = `Total Score: ${totalScore || 0}`;
-        if (totalScore >= 60) {
-            round2ScoreStatus.textContent = "Round 2 Cleared";
-            round2ScoreStatus.style.color = "green";
-        } else {
-            round2ScoreStatus.textContent = "Round 2 Failed";
-            round2ScoreStatus.style.color = "red";
-            round2FailReason.style.display = "block";
-            round2FailReasonContent.textContent = data.reason;
-        }
-        round2TableContainer.style.display = "block";
-    } catch (error) {
-        console.log(error);
-    } finally {
-        setLoading(false);
+    while (round2tbody.firstChild) {
+      round2tbody.removeChild(round1tbody.firstChild);
     }
+
+    parsedData.forEach(function (rowData, index) {
+      const row = document.createElement("tr");
+      const cell = document.createElement("td");
+      cell.textContent = index + 1;
+      row.appendChild(cell);
+      for (const key in rowData) {
+        const cell = document.createElement("td");
+        cell.textContent = rowData[key];
+        row.appendChild(cell);
+      }
+      round2tbody.appendChild(row);
+    });
+
+    round2Score.textContent = `Total Score: ${totalScore || 0}`;
+    if (totalScore >= 60) {
+      round2ScoreStatus.textContent = "Round 2 Cleared";
+      round2ScoreStatus.style.color = "green";
+    } else {
+      round2ScoreStatus.textContent = "Round 2 Failed";
+      round2ScoreStatus.style.color = "red";
+      round2FailReason.style.display = "block";
+      round2FailReasonContent.textContent = data.reason;
+    }
+    round2TableContainer.style.display = "block";
+  } catch (error) {
+    console.log(error);
+  } finally {
+    setLoading(false);
+  }
 });
 //function resetZoom() {
 //  alert('You can reset the zoom level to 100% using your browser settings or keyboard shortcuts:\n- Ctrl + 0 (on Windows/Linux)\n- Cmd + 0 (on macOS)');
